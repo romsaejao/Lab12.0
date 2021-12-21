ad262a088bc02a389a624f4add79914fb9b0cddd
romsaejao <76035596+romsaejao@users.noreply.github.com>
2021-12-21 19:37:00 +0700

    Update lab12_1.cpp

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,47 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main()
+{
+cout << "Press Enter 3 times to reveal your future."<<endl;
+
+cin.ignore();
+cin.ignore();
+cin.ignore();
+
+srand(time(0));
+string g;
+switch(rand()%9+1)
+{
+    case 1: g = "A";
+    cout << "You will get " << g <<" in this 261102.";
+    break;
+    case 2: g = "B";
+    cout << "You will get " << g <<" in this 261102.";
+    break;
+    case 3: g = "B";
+    cout << "You will get " << g <<" in this 261102.";
+    break;
+    case 4: g = "C+";
+    cout << "You will get " << g <<" in this 261102.";
+    break;
+    case 5: g = "C";
+    cout << "You will get " << g <<" in this 261102.";
+    break;
+    case 6: g = "D+";
+    cout << "You will get " << g <<" in this 261102.";
+    break;
+    case 7: g = "D";
+    cout << "You will get " << g <<" in this 261102.";
+    break;
+    case 8: g = "F";
+    cout << "You will get " << g <<" in this 261102.";
+    break;
+    case 9: g = "W";
+    cout << "You will get " << g <<" in this 261102.";
+    break;
+    
+}
+
+}
